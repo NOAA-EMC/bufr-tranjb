66d8d87606f06415e6dadc7de11ff786337dd302
Rahul Mahajan <aerorahul@users.noreply.github.com>
2021-10-20 18:59:59 +0000

    clean up before PR

--- build.sh
+++ build.sh
@@ -13,28 +13,11 @@ pkg_root=`dirname $(${readlink} -f $0)`
 target=$(echo $target | tr [:upper:] [:lower:])
 if [[ "$target" =~ ^(wcoss2|hera|orion)$ ]]; then
   source $pkg_root/versions/build.ver
-  if [[ ! "$target" =~ ^(wcoss2)$ ]]; then
-    # WCOSS2 wants these hardwired in build.ver
-    # This makes the build and porting inflexible
-    # as well as this script to have if-blocks for WCOSS2
-    # These variables have no meaning on any other machine
-    unset intel_ver
-    unset craype_ver
-    unset cray_mpich_ver
-  fi
   set +x
   module use $pkg_root/modulefiles
   module load bufrtranjb_$target
   module list
   set -x
-  # WCOSS2 likes to call the bin/ directory exec/
-  if [[ "$target" =~ ^(wcoss2)$ ]]; then
-    CMAKE_OPTS+=" -DCMAKE_INSTALL_BINDIR=exec"
-    # Needs bugfix in nceplibs-bufr develop that is not in 11.5.0
-    export bufr_ROOT=/lfs/h2/emc/eib/noscrub/Rahul.Mahajan/tmp_stack/nceplibs-bufr/install
-    # Needs w3emc 2.9.1 that eliminates w3nco dependency
-    export w3emc_ROOT=/lfs/h2/emc/eib/noscrub/Rahul.Mahajan/tmp_stack/nceplibs-w3emc/install
-  fi
 fi
 
 # Determine install path.
--- modulefiles/bufrtranjb_wcoss2.lua
+++ modulefiles/bufrtranjb_wcoss2.lua
@@ -6,7 +6,7 @@ intel_ver=os.getenv("intel_ver") or "default"
 
 load("envvar")
 load("PrgEnv-intel")
-load(pathJoin("intel", intel_ver))
+load(pathJoin("intel/19.1.3.304"))
 
 -- Load common modules for this package
 load("bufrtranjb_common")
