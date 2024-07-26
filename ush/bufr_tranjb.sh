50ed7cfd35931e5763b67837b42d0a9b7dd3caa4
Iliana Genkova <iliana.genkova@noaa.gov>
2024-07-26 16:39:06 +0000

    Set rtank_tbl in /modulefiles; prepare for setting it in /ush/bufr_tranjb.sh with next implementation

--- ush/bufr_tranjb.sh
+++ ush/bufr_tranjb.sh
@@ -435,11 +435,11 @@ host=$(hostname -s)
 
 tank_dir=$1
 bufrfile=$2
+
 #####################################
-### Set up restricted tanks table - DF may overwrite
+### Set up restricted tanks table - Decoders and DF may overwrite
 ######################################
-export envir=${envir:-prod}
-export rtank_tbl=${rtank_tbl:-/apps/ops/${envir}/nco/core/bufr_tranjb.v${bufr_tranjb_ver}/dictionaries/rstprod_bufr_tank.tbl}
+#export rtank_tbl=${rtank_tbl:-/apps/ops/prod/nco/core/bufr_tranjb.v${bufr_tranjb_ver}/dictionaries/rstprod_bufr_tank.tbl}
 
 #  unset `env | grep "^FORT[0-9]\{1,\}=" | awk -F= '{print $1}'`
 
